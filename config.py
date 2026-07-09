48053b8d0ac3e93ce4260071386363b85986d5c9
2ahmedhassan2 <111598403+2ahmedhassan2@users.noreply.github.com>
2026-07-09 16:29:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.getenv("BOT_TOKEN", "ضع_توكن_البوت_هنا")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+TOKEN = os.getenv("BOT_TOKEN", "8649992184:AAES8IEZiLph55200eClirtuSyM5TOiSWWE")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "1679372391"))
 DATABASE_NAME = "database.db"
 CACHE_TIMEOUT = 600  # مدة الكاش بالثواني (10 دقائق)
 RATE_LIMIT_COUNT = 50  # الحد الأقصى للطلبات في الدقيقة لمنع السبام
